c72a75c97b0e089324ec9995d12c8f14e21636f4
yawgad23 <2.82677219e+08+yawgad23@users.noreply.github.com>
2026-09-22 19:39:59 +0000

    chore: bump rider build for Railway backend

--- app.config.ts
+++ app.config.ts
@@ -56,7 +56,7 @@ const config: ExpoConfig = {
   ios: {
     supportsTablet: true,
     bundleIdentifier: env.iosBundleId,
-    buildNumber: "34",
+    buildNumber: "35",
     "infoPlist": {
         "ITSAppUsesNonExemptEncryption": false
       }
