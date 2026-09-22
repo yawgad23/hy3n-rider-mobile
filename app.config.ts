155562ff40be11172d7afe7c84d465938d6ac2a3
yawgad23 <2.82677219e+08+yawgad23@users.noreply.github.com>
2026-09-22 21:34:27 +0000

    chore: increment iOS TestFlight build

--- app.config.ts
+++ app.config.ts
@@ -56,7 +56,7 @@ const config: ExpoConfig = {
   ios: {
     supportsTablet: true,
     bundleIdentifier: env.iosBundleId,
-    buildNumber: "36",
+    buildNumber: "37",
     "infoPlist": {
         "ITSAppUsesNonExemptEncryption": false
       }
