99c24871b17fb28ea5cd8de970481c7de545692a
yawgad23 <2.82677219e+08+yawgad23@users.noreply.github.com>
2026-09-22 16:20:19 +0000

    fix: increment rider iOS TestFlight build number

--- app.config.ts
+++ app.config.ts
@@ -56,6 +56,7 @@ const config: ExpoConfig = {
   ios: {
     supportsTablet: true,
     bundleIdentifier: env.iosBundleId,
+    buildNumber: "28",
     "infoPlist": {
         "ITSAppUsesNonExemptEncryption": false
       }
