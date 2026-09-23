70eb28b8ad68e896e34f178f3f23bc90ca33ff2c
yawgad23 <2.82677219e+08+yawgad23@users.noreply.github.com>
2026-09-23 17:02:23 +0000

    chore: set Rider iOS build 45

--- app.config.ts
+++ app.config.ts
@@ -56,7 +56,7 @@ const config: ExpoConfig = {
   ios: {
     supportsTablet: true,
     bundleIdentifier: env.iosBundleId,
-    buildNumber: "44",
+    buildNumber: "45",
     "infoPlist": {
         "ITSAppUsesNonExemptEncryption": false
       }
