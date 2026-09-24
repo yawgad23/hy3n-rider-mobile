94f9d7f184db80e6d3a518d6be447a984b72a27e
yawgad23 <2.82677219e+08+yawgad23@users.noreply.github.com>
2026-09-24 16:41:14 +0000

    build(rider): reserve iOS build 54

--- app.config.ts
+++ app.config.ts
@@ -56,7 +56,7 @@ const config: ExpoConfig = {
   ios: {
     supportsTablet: true,
     bundleIdentifier: env.iosBundleId,
-    buildNumber: "53",
+    buildNumber: "54",
     googleServicesFile: "./firebase/GoogleService-Info.plist",
     "infoPlist": {
         "ITSAppUsesNonExemptEncryption": false,
