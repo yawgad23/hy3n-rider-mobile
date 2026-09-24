6a1a3544c1ade6df7e3ff2c3a5618d850d6a8e3e
yawgad23 <2.82677219e+08+yawgad23@users.noreply.github.com>
2026-09-24 11:32:03 +0000

    chore(release): reserve rider iOS build 50

--- app.config.ts
+++ app.config.ts
@@ -56,7 +56,7 @@ const config: ExpoConfig = {
   ios: {
     supportsTablet: true,
     bundleIdentifier: env.iosBundleId,
-    buildNumber: "49",
+    buildNumber: "50",
     "infoPlist": {
         "ITSAppUsesNonExemptEncryption": false
       }
